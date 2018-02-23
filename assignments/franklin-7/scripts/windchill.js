15446638b4cc4ed046ee9655fd00b64950f141c2
Samantha Daly <35244487+samdaly130@users.noreply.github.com>
2018-02-23 13:46:58 -0500

    fixed windchill

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,3 +1,16 @@
-var t = (90 + 66) / 2;
-var s = 5;
-document.getElementsByClassName("windchill").innerHTML = 35.74 + 0.6215 * (t - 35.75) * (Math.pow(s, 0.16)) + (0.4275 * t) * (Math.pow(s, 0.16));
+function doInputOutput() {
+
+    var t = (90 + 66) / 2; 
+    var s = 5;
+    var result = windChill(t, s);
+    
+    document.getElementById('outputDiv').innerHTML = Math.round(result);
+}
+
+function windChill(tempF, speed) {
+    
+    var f = 35.74 + 0.6215 * tempF - 35.75 * (Math.pow(speed, 0.16)) + 0.4275 * tempF * (Math.pow(speed, 0.16));
+    
+    return f;
+}
+
