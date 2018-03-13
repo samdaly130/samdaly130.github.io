adeeba69d0851dd9f3b8be84a38722db383d3a8e
Samantha Daly <35244487+samdaly130@users.noreply.github.com>
2018-03-13 15:59:11 -0400

    Start of Final Weather Site

---
+++ assignments/weathersite/scripts/weather.js
@@ -0,0 +1,52 @@
+/*-------API KEY: 2484b1bb70021030 ------*/
+var section = document.getElementById('jsonSec');
+
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+   var weatherMan = request.response;
+    showData(weatherMan);
+}
+
+function showData(jsonObj) {
+    var towns = jsonObj['towns'];
+    
+    for (var i = 0; i < towns.length; i++) {
+        
+        var article = document.createElement('article');
+        var h2 = document.createElement('h2');
+        var motto = document.createElement('p');
+        var yearFounded = document.createElement('p');
+        var currentPopulation = document.createElement('p');
+        var averageRainfall = document.createElement('p');
+        var events = document.createElement('ul');
+        
+        h2.textContent = towns[i].name;
+        motto.textContent = 'Motto: ' + towns[i].motto;
+        yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        currentPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+        averageRainfall.textContent = 'Average RainFall: ' + towns[i].averageRainfall;
+        events.textContent = 'Events: ';
+        
+        var eventList = towns[i].events;
+        for (var j = 0; j < eventList.length; j++) {
+            var listItem = document.createElement('li');
+            listItem.textContent = eventList[j];
+            events.appendChild(listItem);
+        }
+        
+        article.appendChild(h2);
+         article.appendChild(motto);
+         article.appendChild(yearFounded);
+         article.appendChild(currentPopulation);
+         article.appendChild(averageRainfall);
+         article.appendChild(events);
+        
+        section.appendChild(article);
+        
+    }
+}
---
+++ assignments/weathersite/scripts/wu-weather-greenville.js
@@ -0,0 +1,39 @@
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET','//api.wunderground.com/api/2484b1bb70021030/conditions/q/TX/Greenville.json',true);
+
+weatherObject.send();
+
+weatherObject.onload = function() {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+
+
+document.getElementById('conditions').innerHTML = weatherInfo.current_observation.weather;
+    
+document.getElementById('temperature').innerHTML = weatherInfo.current_observation.temp_f;
+    
+document.getElementById('windS').innerHTML = weatherInfo.current_observation.wind_mph;
+
+document.getElementById('weather_icon').src = weatherInfo.current_observation.icon_url;
+    
+}
+
+var forecastObject = new XMLHttpRequest();
+
+forecastObject.open('GET','//api.wunderground.com/api/2484b1bb70021030/forecast/q/TX/Greenville.json',true);
+
+forecastObject.send();
+
+forecastObject.onload = function() {
+    var forecastInfo = JSON.parse(forecastObject.responseText);
+    console.log(forecastInfo);
+    
+    document.getElementById('forecast').innerHTML = 'Forecast: ' + forecastInfo.forecast.simpleforecast.forecastday["0"].conditions;
+    
+    document.getElementById('lows').innerHTML = 'Lows overnight around ' + forecastInfo.forecast.simpleforecast.forecastday["0"].low.fahrenheit + '&deg;';
+    
+}
+    
+
+
+
