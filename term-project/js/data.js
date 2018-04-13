0ba984609b19c79954d0a3fc6d778c0d34a9e1c9
Samantha Daly <35244487+samdaly130@users.noreply.github.com>
2018-04-13 13:27:50 -0400

    json

--- term-project/js/data.js
+++ term-project/js/data.js
@@ -1,48 +1,15 @@
-         var request = new XMLHttpRequest();
-         request.open('GET', 'https://samdaly130.github.io/term-project/data.json',true);
+         var serviceObject = new XMLHttpRequest();
+         serviceObject.open('GET', 'https://samdaly130.github.io/term-project/data.json',true);
 
-            request.send();
+            serviceObject.send();
             
-            request.onload = function() {
-                var services = JSON.parse(request.responseText);
-                console.log(services)
-                section(services);
-                table(services);
+            serviceObject.onload = function() {
+                var serviceInfo = JSON.parse(serviceObject.responseText);
+                console.log(serviceInfo);
+                
+                document.getElementById('header').innerHTML = serviceInfo.head;
+                
+                document.getElementById('headlineOne').innerHTML = serviceInfo.table
+                
             } 
             
-            function section(jsonObj) {
-                
-                var h1 = document.createElement('h1');
-                 h1.textContent = jsonObj['.head'];
-                section.appendChild(h1);
-            }
-            
-            function table(jsonObj) {
-                
-                var s3rvices = jsonObj['.table'];
-                
-                for (var i = 0; i < s3rvices.length; i++) {
-                    var cases = document.createElement('tr');
-                    var headline1 = document.createElement('th');
-                    var headline2 = document.createElement('th');
-                    
-                    headline1.innerHTML = .table['0'].services;
-                    headline2.textContent = .table['1'].prices;
-                    
-                        var content = s3rvices[i].services;
-                        for (var s = 0; s < content.length; s++) {
-                            var bikeServ = document.createElement('td');
-                            bikeServ.textContent = content[s];
-                            cases.appendChild(bikeServ);
-                        }
-                    var pric3s = s3rvices[i].prices;
-                    for (var p = 0; p < pric3s.length; p++) {
-                        var pricing = document.createElement('td');
-                        pricing.textContent = pric3s[p];
-                        cases.appendChild(pricing);
-                    }
-                    cases.appendChild(headline1);
-                    cases.appendChild(headline2);
-table.appendChild(cases);
-                }
-            }
