63270f338ae1a6f63cec572accbeef51f5e95b63
Samantha Daly <35244487+samdaly130@users.noreply.github.com>
2018-04-13 18:34:15 -0400

    json improvements

--- term-project/js/data.js
+++ term-project/js/data.js
@@ -9,7 +9,15 @@
                 
                 document.getElementById('header').innerHTML = serviceInfo.head;
            
-                document.getElementById('services').innerHTML = serviceInfo.table["0"].services;
+                document.getElementById('services1').innerHTML = serviceInfo.table["0"].services["0"];
+                document.getElementById('services2').innerHTML = serviceInfo.table["0"].services["1"];
+                document.getElementById('services3').innerHTML = serviceInfo.table["0"].services["2"];
+                document.getElementById('services4').innerHTML = serviceInfo.table["0"].services["3"];
+                document.getElementById('services5').innerHTML = serviceInfo.table["0"].services["4"];
+                document.getElementById('services6').innerHTML = serviceInfo.table["0"].services["5"];
+                document.getElementById('services7').innerHTML = serviceInfo.table["0"].services["6"];
+                document.getElementById('services8').innerHTML = serviceInfo.table["0"].services["7"];
+                
                     
                 }
                 
